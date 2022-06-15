3ac11b1a16ec9e31c5cb96b6bef8f9adc6349cb0
isonguyom <isonguyom@gmail.com>
2022-06-15 16:49:34 +0100

    more styling and create js folder

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// let regForm = document.getElementsByTagName("form");
+// let submitBtn = document.getElementById("submitBtn");
+
+
+// submitBtn.addEventListener("submit", function() {
+//     regForm.attribute.v
+// })
